bf785c26aa55598ebebfdd4959614b8aecc4c466
Breno Ryan <brenoryan21@hotmail.com>
2023-10-17 21:59:22 -0300

    feat: Implementing Publish Message

--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -2,6 +2,7 @@ package com.ms.user.services;
 
 import com.ms.user.dto.UserDTO;
 import com.ms.user.models.UserModel;
+import com.ms.user.produces.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ public class UserService {
     @Autowired
     UserRepository repository;
 
+    @Autowired
+    UserProducer userProducer;
+
     @Transactional
     public UserModel saveUser(UserDTO userDTO){
 
@@ -20,7 +24,7 @@ public class UserService {
         user.setEmail(userDTO.email());
         user.setName(userDTO.name());
         repository.save(user);
-
+        userProducer.publishMessageEmail(user);
         return user;
     }
 }
